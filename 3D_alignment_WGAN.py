f3bbb5546cc360386f32ed6c336e9682563ea981
JimmyGoo <guzc14@mails.tsinghua.edu.cn>
2017-07-26 22:25:59 -0500

    fix FIFO bug

--- 3D_alignment_WGAN.py
+++ 3D_alignment_WGAN.py
@@ -3,15 +3,24 @@ from tensorflow import layers as ly
 import tensorflow.contrib.layers as cly
 from util import *
 from functools import partial
+from sys import argv
+
+os.environ["CUDA_VISIBLE_DEVICES"]=argv[1]
+###Problem
+
+##8 * 8 * 8 in voxel space?
+##giving output S = 64 64 64, how to back in 8 * 8 * 8(S value is not int, S maybe larger then 8 * 8 * 8, value in S dont have
+##corresponding index)
 
 xavier_init = cly.xavier_initializer()
 #input 10(pending) dim Z latent space
 #Pending
-batch_size = 15
+batch_size = 5
 z_size = 10
 
 learning_rate_gen = 5e-5
 learning_rate_dis = 5e-5
+shape_size = [13,13,13,3]
 
 max_iter_step = 2000
 
@@ -25,7 +34,7 @@ Citers = 5
 log_path = './log/chair/'
 record_path = './data/tfrecord/'
 
-n_epoch = 10
+n_epoch = 5
 
 clamp_lower = -0.01
 clamp_upper = 0.01
@@ -35,7 +44,7 @@ def generator(z, batch_size=batch_size,phase_train=True, ):
 	#Pending
 	stride_g = [2,2,2]
 	kernel_g1 = [4,4,4]
-	kernel_g2 = [8,8,8]
+	kernel_g2 = [8,8,8] 
 	kernel_g3 = [16,16,16]
 	kernel_g4 = [32,32,32]
 	kernel_g5 = [64,64,64]
@@ -44,36 +53,35 @@ def generator(z, batch_size=batch_size,phase_train=True, ):
 		print "z shape: ", z.shape
 
 		net = tf.reshape(z, (batch_size,1,1,1,z_size))
-		g1 = ly.conv3d_transpose(inputs=net, filters=512, kernel_size=kernel_g1, strides=stride_g
+		g1 = ly.conv3d_transpose(inputs=net, filters=384, kernel_size=kernel_g1, strides=stride_g
 			, padding="VALID", activation=tf.nn.relu, activity_regularizer=cly.batch_norm, 
 			kernel_initializer=xavier_init)
 
 		print "g1 shape: ", g1.shape
 
-		g2 = ly.conv3d_transpose(inputs=g1, filters=256, kernel_size=kernel_g2, strides=stride_g
+		g2 = ly.conv3d_transpose(inputs=g1, filters=192, kernel_size=kernel_g2, strides=stride_g
 			, padding="SAME", activation=tf.nn.relu, activity_regularizer=cly.batch_norm, 
 			kernel_initializer=xavier_init)
 
 		print "g2 shape: ", g2.shape
 
-		g3 = ly.conv3d_transpose(inputs=g2, filters=128, kernel_size=kernel_g3, strides=stride_g
-			, padding="SAME", activation=tf.nn.relu, activity_regularizer=cly.batch_norm, 
-			kernel_initializer=xavier_init)
+		g3 = ly.conv3d_transpose(inputs=g2, filters=3, kernel_size=kernel_g3, strides=stride_g
+			, padding="SAME", activation=tf.nn.tanh, kernel_initializer=xavier_init)
 
 		print "g3 shape: ", g3.shape
 
-		g4 = ly.conv3d_transpose(inputs=g3, filters=64, kernel_size=kernel_g4, strides=stride_g
-			, padding="SAME", activation=tf.nn.relu, activity_regularizer=cly.batch_norm, 
-			kernel_initializer=xavier_init)
+		# g4 = ly.conv3d_transpose(inputs=g3, filters=64, kernel_size=kernel_g4, strides=stride_g
+		# 	, padding="SAME", activation=tf.nn.relu, activity_regularizer=cly.batch_norm, 
+		# 	kernel_initializer=xavier_init)
 
-		print "g4 shape: ", g4.shape
+		# print "g4 shape: ", g4.shape
 
-		g5 = ly.conv3d_transpose(inputs=g4, filters=1, kernel_size=kernel_g5, strides=stride_g
-			, padding="SAME", activation=tf.nn.tanh, kernel_initializer=xavier_init)
+		# g5 = ly.conv3d_transpose(inputs=g4, filters=3, kernel_size=kernel_g5, strides=stride_g
+		# 	, padding="SAME", activation=tf.nn.tanh, kernel_initializer=xavier_init)
 
-		print "g5 shape: ", g5.shape
+		# print "g5 shape: ", g5.shape
 
-	return g5
+	return g3
 
 def discriminator(inputs, phase_train=True, reuse=False):
 	stride_d = [2,2,2]
@@ -86,52 +94,46 @@ def discriminator(inputs, phase_train=True, reuse=False):
 
 		print "inputs shape: ", inputs.shape
 
-		d1 = ly.conv3d(inputs=inputs, filters=64, kernel_size=kernel_d,
+		d1 = ly.conv3d(inputs=inputs, filters=3, kernel_size=kernel_d,
 			strides=stride_d, padding="SAME",activation=leaky_relu, activity_regularizer=cly.batch_norm,
 			kernel_initializer=xavier_init)
 
 		print "d1 shape: ", d1.shape
 
-		d2 = ly.conv3d(inputs=d1, filters=128, kernel_size=kernel_d,
+		d2 = ly.conv3d(inputs=d1, filters=192, kernel_size=kernel_d,
 			strides=stride_d, padding="SAME",activation=leaky_relu, activity_regularizer=cly.batch_norm,
 			kernel_initializer=xavier_init)
 
 		print "d2 shape: ", d2.shape
 
-		d3 = ly.conv3d(inputs=d2, filters=256, kernel_size=kernel_d,
+		d3 = ly.conv3d(inputs=d2, filters=384, kernel_size=kernel_d,
 			strides=stride_d, padding="SAME",activation=leaky_relu, activity_regularizer=cly.batch_norm,
 			kernel_initializer=xavier_init)
 
 		print "d3 shape: ", d3.shape
 
-		d4 = ly.conv3d(inputs=d3, filters=512, kernel_size=kernel_d,
-			strides=stride_d, padding="SAME",activation=leaky_relu, activity_regularizer=cly.batch_norm,
-			kernel_initializer=xavier_init)
+		# d4 = ly.conv3d(inputs=d3, filters=512, kernel_size=kernel_d,
+		# 	strides=stride_d, padding="SAME",activation=leaky_relu, activity_regularizer=cly.batch_norm,
+		# 	kernel_initializer=xavier_init)
+
+		# print "d4 shape: ", d4.shape
+
+		d4 = cly.fully_connected(tf.reshape(
+            d3, [batch_size, -1]), 1, activation_fn=None)
 
 		print "d4 shape: ", d4.shape
 
-		d5 = ly.conv3d(inputs=d4, filters=1, kernel_size=kernel_d,
-			strides=stride_d5, padding="VALID",activation=tf.nn.sigmoid, 
-			kernel_initializer=xavier_init)
+	return d4
 
-		print "d5 shape: ", d5.shape
-
-	return d5
-
-def build_graph():
+def build_graph(real_cp):
 
 	noise_dist = tf.contrib.distributions.Normal(0., 1.)
 	z = noise_dist.sample((batch_size, z_size))
 	gen = generator
 	dis = discriminator
 
-	# BSCoeff_path = './'
- # 	BSCoeff = loadBscoeff(BSCoeff_path):
-
 	with tf.variable_scope('generator'):
 		fake_cp = generator(z)
-		real_cp = tf.placeholder(
-		dtype=tf.float32, shape=(batch_size, 64, 64, 64, 1))
 
 	true_logit = dis(real_cp)
 	fake_logit = dis(fake_cp, reuse=True)
@@ -141,11 +143,6 @@ def build_graph():
 
 	g_loss_sum = tf.summary.scalar("g_loss", g_loss)
 	d_loss_sum = tf.summary.scalar("c_loss", d_loss)
-
-
-	# fake_shape = deform(, fake_cp)
-	fake_shape = fake_cp
-	_sum = tf.summary.image("img", fake_shape, max_outputs=10)
 
 	theta_g = tf.get_collection(
 		tf.GraphKeys.TRAINABLE_VARIABLES, scope="generator")
@@ -170,56 +167,64 @@ def build_graph():
 		opt_d = tf.tuple(clipped_var_d)
 
 
-	return opt_g, opt_d, real_cp
+	return opt_g, opt_d
 
 def main():
-	cp_batch = load_data(record_path, n_epoch, batch_size)
-
-	with tf.device(device_gpu):
-		opt_g, opt_d, real_cp = build_graph()
-
 	merged_all = tf.summary.merge_all()
-	saver = tf.train.Saver()
 	config = tf.ConfigProto(allow_soft_placement=True, log_device_placement=True)
 
 	config.gpu_options.allow_growth = True
 	config.gpu_options.per_process_gpu_memory_fraction = 0.8
 	
 	with tf.Session(config=config) as sess:
-		sess.run(tf.global_variables_initializer())
+		coord = tf.train.Coordinator()
+		threads = tf.train.start_queue_runners(sess=sess, coord=coord)
 		summary_writer = tf.summary.FileWriter(log_path, sess.graph)
-		feed_dict =  {real_cp: cp_batch.eval()}
+	
+		cp_batch = load_data(record_path, n_epoch, batch_size, tuple(shape_size))
+
+		with tf.device(device_gpu):
+			opt_g, opt_d = build_graph(cp_batch)
+
+		sess.run(tf.global_variables_initializer())
+
+		print "Finish Building Graph"
+		print "Finish Init and Start Training Step"
 
 		for i in range(max_iter_step):
 			if i < 25 or i % 500 == 0:
 				citers = 100
 			else:
 				citers = Citers
+
+			print "step: %r of total step %r" % (i, max_iter_step)
+
 			for j in range(citers):
-				
-				if i % 100 == 99 and j == 0:
+				print "citers %r of %r during step %r" % (j, citers, i)
+ 				if i % 100 == 99 and j == 0:
 					run_options = tf.RunOptions(
 						trace_level=tf.RunOptions.FULL_TRACE)
 					run_metadata = tf.RunMetadata()
-					_, merged = sess.run([opt_d, merged_all], feed_dict=feed_dict,
+					_, merged = sess.run([opt_d, merged_all],
 										 options=run_options, run_metadata=run_metadata)
+
 					summary_writer.add_summary(merged, i)
 					summary_writer.add_run_metadata(
 						run_metadata, 'critic_metadata {}'.format(i), i)
 				else:
-					sess.run(opt_d, feed_dict=feed_dict)
+					sess.run(opt_d)
 
 			if i % 100 == 99:
-				_, merged = sess.run([opt_g, merged_all], feed_dict=feed_dict,
+				_, merged = sess.run([opt_g, merged_all],
 					 options=run_options, run_metadata=run_metadata)
 				summary_writer.add_summary(merged, i)
 				summary_writer.add_run_metadata(
 					run_metadata, 'generator_metadata {}'.format(i), i)
 			else:
-				sess.run(opt_g, feed_dict=feed_dict)                
+				sess.run(opt_g)                
 			if i % 1000 == 999:
 				saver.save(sess, os.path.join(
 					ckpt_dir, "model.ckpt"), global_step=i)
 
 if __name__ == '__main__':
-    main()
+	main()
--- util.py
+++ util.py
@@ -7,18 +7,7 @@ def leaky_relu(x, leak=0.3, name="lrelu"):
 	with tf.variable_scope(name):
 		return tf.maximum(x, x * leak)
 
-def deform(BSCoeff_path, dstCP):
-	deform_shape = tf.matmul(BSCoeff_path, dstCP)
-	return deform_shape
-
-def loadBscoeff(BSCoeff_path):
-	#to DO
-	BSCoeff = sio.loadmat(BSCoeff_path)
-	BSCoeff = BSCoeff['BSCoeff']
-	shape = None
-	return tf.constant(BSCoeff_path)
-
-def read_and_decode(queue, D, W, H):
+def read_and_decode(queue, size):
 	reader = tf.TFRecordReader()
 	_, serialized_example = reader.read(queue)
 	# get feature from serialized example
@@ -30,16 +19,17 @@ def read_and_decode(queue, D, W, H):
 		}
 	)
 	shape = features['dstCP']
-	shape = tf.decode_raw(shape, tf.uint8)
-	shape = tf.reshape(shape, [D, W, H, 1])
-	shape = tf.cast(shape, tf.float32)
+	shape = tf.decode_raw(shape, tf.float32)
+	shape = tf.reshape(shape, size)
+
 	return shape
 
-def load_data(record_path, n_epoch, batch_size):
+def load_data(record_path, n_epoch, batch_size, shape_size):
 	filenames = [record_path + name for name in os.listdir(record_path) if name != '.DS_Store']
 	print "loading tfrecord: ", filenames
-	filename_queue = tf.train.string_input_producer(filenames, num_epochs=n_epoch)
-	shape = read_and_decode(filename_queue, 64, 64, 64)
+	filename_queue = tf.train.string_input_producer(filenames)
+	shape = read_and_decode(filename_queue, shape_size)
+	
 	shape_batch = tf.train.batch(
 	  [shape], batch_size=batch_size)
 	return shape_batch
