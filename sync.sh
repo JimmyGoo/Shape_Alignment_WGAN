41f80c011bd232e81c16979211b5f81de2832c68
JimmyGoo <guzc14@mails.tsinghua.edu.cn>
2017-07-31 15:24:24 -0500

    add component

---
+++ sync.sh
@@ -0,0 +1,1 @@
+rsync -av --progress --rsh='ssh' --exclude ".DS_Store" --exclude "model/" --exclude "vis/" --exclude "temp.py" --exclude "create_tfrecord_old.py" --exclude "data/" --exclude "*.pyc" --exclude "log/" -r ./ yangqingzhu@166.111.7.190:~/jimmygoo/3D_Alignment_WGAN/
